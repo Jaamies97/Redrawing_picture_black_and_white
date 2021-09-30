b12819cdcb37d5e5d69e695f4e089097e0d44769
Jaamies97 <axel.ojutkangas@gmail.com>
2021-10-01 01:32:25 +0300

    Makes a clickable button

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QPushButton, QBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QPushButton, QBoxLayout, QGroupBox, QGridLayout
 from PyQt5.QtCore import QDir
+
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.resize(700,500)
+        self.resize(500, 100)
+        self.downloadButton()
+        self.setWindowTitle("Redrawing the picture")
+        layout = QGridLayout()
+        layout.addWidget(self.buttonLayout)
+        self.setLayout(layout)
+
+
+    def downloadButton(self):
+        self.buttonLayout = QGroupBox("Push button to select a file")
+        settingDownloadB = QPushButton('Open a file',self)
+        settingDownloadB.clicked.connect(self.openSelection)
+        layout = QGridLayout()
+        layout.addWidget(settingDownloadB)
+        self.buttonLayout.setLayout(layout)
+        settingDownloadB
+    def openSelection(self):
+        pass
 
 
 def main():
@@ -14,6 +32,7 @@ def main():
     demo.show()
 
     sys.exit(app.exec_())
+
 
 if __import__("__main__"):
     main()
